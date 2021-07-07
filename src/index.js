bdc7db6defc5a85df0709f57ede09d6409d02632
Gaurav Shankar <gaurav.shankar@talentica.com>
2021-07-07 16:21:41 +0530

    Added Tailwind

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import * as styles from './shared/styles';
 import * as global from './shared/global';
 import * as animation from './shared/animation';
 import * as icons from './shared/icons';
-
+import './shared/tailwind.css';
 export { styles, global, animation, icons };
 
-export * from './Avatar';
-export * from './Badge';
-export * from './Button';
-export * from './Icon';
-export * from './Link';
+export * from './atoms/Avatar';
+export * from './atoms/Badge';
+export * from './atoms/Button';
+export * from './atoms/Icon';
+export * from './atoms/Link';
