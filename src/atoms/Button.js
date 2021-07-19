69822da6a58078b3d5360a543354c2941b1ba484
Shweta Deodhare <shweta.deodhare@talentica.com>
2021-07-19 15:25:09 +0530

    Updated: Button Component.

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const APPEARANCES = {
-  PRIMARY: 'primary',
-  SECONDARY: 'secondary',
+const VARIENTS = {
+  SOLID: 'solid',
+  OUTLINE: 'outline',
+  SOLID_PILL: 'solid-pill',
+  OUTLINED_PILL: 'outline-pill',
 };
 
 const SIZES = {
@@ -16,17 +18,32 @@ const CORNER_RADIUS = {
   pill: 'rounded-full'
 }
 
+const COLOR = {
+  primary: 'primary',
+  secondary: 'secondary'
+}
+
 export const Button = (props) => {
-	const {className = '', color = 'primary', isDisabled = false, cornerRadius, isLoading, loadingText, iconClassName = ''} = props
-	const {label="Button", outline = false, size = '', type = 'button', onClick, title} = props;
-  //Set Outline Button as per theme
-  let isOutlineButton ='';
-  isOutlineButton = (outline ? `border-theme-${color} text-theme-${color} hover:bg-theme-${color} hover:text-white` : `bg-theme-${color} text-white hover:bg-opacity-90 border-theme-${color}`)
+	const { isLink, label="Button", size, type = 'button', onClick, className, color, varient, isDisabled = false, cornerRadius, isLoading, loadingText, title} = props
+
+  let buttonVarient;
+  if (varient === "solid") {
+    buttonVarient = `bg-theme-${color} text-white hover:bg-opacity-90 border-theme-${color} rounded`;
+  } else if (varient === "outline") {
+    buttonVarient = `border-theme-${color} text-theme-${color} hover:bg-theme-${color} hover:text-white rounded`;
+  } else if (varient === "solid-pill") {
+    buttonVarient = `bg-theme-${color} text-white hover:bg-opacity-90 border-theme-${color} rounded-full`;
+  } else if (varient === "outline-pill") {
+    buttonVarient = `border-theme-${color} text-theme-${color} hover:bg-theme-${color} hover:text-white rounded-full`;
+  } else {
+    buttonVarient = `bg-theme-${color} text-white hover:bg-opacity-90 border-theme-${color} rounded`;
+  }
+
   let buttonDisabled = '';
-  buttonDisabled = (isDisabled ? 'cursor-not-allowed bg-opacity-65 ' : '');
+  buttonDisabled = (isDisabled ? 'cursor-not-allowed hover:bg-opacity-60 bg-opacity-60 border-opacity-10 text-opacity-60	 ' : '');
 
   let buttonClassName = '';
-  buttonClassName = ( `${isOutlineButton} ${buttonDisabled} ` + (size === 'small' ? 'px-5 py-3 ' : 'px-8 py-4 ') + `${cornerRadius} ${className}` );
+  buttonClassName = ( `${buttonVarient} ${buttonDisabled} ` + (size === 'small' ? 'px-5 py-3 ' : 'px-8 py-4 ') + `${cornerRadius} ${className}` );
 
   const buttonInner = (
     <Fragment>
@@ -35,6 +52,13 @@ export const Button = (props) => {
     </Fragment>
   );
 
+  if(isLink){
+    return(
+      <a title={title} className={`${buttonClassName} border`}>
+        {buttonInner}
+      </a>
+    )
+  }
 	return (
 		<button
 			type={type}
@@ -51,46 +75,34 @@ export const Button = (props) => {
 
 
 Button.propTypes = {
+  title: PropTypes.any,
+  varient:PropTypes.oneOf(Object.values(VARIENTS)),
+  cornerRadius: PropTypes.oneOf(Object.values(COLOR)),
   isLoading: PropTypes.bool,
-  
-   /** When a button is in the loading state you can supply custom text
-  */
+  /** When a button is in the loading state you can supply custom text */
  loadingText: PropTypes.node,
-  
-   /** Buttons that have hrefs should use <a> instead of <button>
-  */
-  // isLink: PropTypes.bool,
-  /**
-    What label button has
-  */
+  /** Buttons that have hrefs should use <a> instead of <button>*/
+  isLink: PropTypes.bool,
+
+  /** What label button has */
   label: PropTypes.node.isRequired,
-  /**
-    Buttons that have outline and fills on hover
-  */
+  /** Buttons that have outline and fills on hover */
   outline: PropTypes.bool,
   isDisabled: PropTypes.bool,
-  /**
-   Prevents users from clicking on a button multiple times (for things like payment forms)
-  */
-  // isUnclickable: PropTypes.bool,
-  /**
-   Buttons with icons by themselves have a circular shape
-  */
-  // containsIcon: PropTypes.bool,
+
+  /** Buttons having different sizes */
   size: PropTypes.oneOf(Object.values(SIZES)),
   // ButtonWrapper: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
   cornerRadius: PropTypes.oneOf(Object.values(CORNER_RADIUS)),
 };
 
 Button.defaultProps = {
+  color: COLOR.primary,
+  varient:VARIENTS.solid,
   isLoading: false,
   loadingText: null,
   isLink: false,
   isDisabled: false,
-  isUnclickable: false,
-  containsIcon: false,
   size: SIZES.MEDIUM,
-  ButtonWrapper: undefined,
-  outline: false,
   cornerRadius: CORNER_RADIUS.rounded,
 };
--- src/atoms/Button.stories.js
+++ src/atoms/Button.stories.js
@@ -5,13 +5,18 @@ import { Button } from './Button';
 export default {
   title: 'Design System/Atoms/Button',
   argTypes: {
-      color: { control: { type: "select", options: ["primary", "secondary"] },
-      cornerRadius: { control: { type: "select", options: ["rounded", "pill"] }},
+      color: {
+        options: ['primary', 'secondary'],
+        control: { type: 'radio' }
+      },
+      size: {
+        options: ['small', 'medium'],
+        control: { type: 'radio' }
+      },
+      varient: { control: { type: "select", options: ["solid", "outline", "solid-pill", "outline-pill"] },
       className: 'mr-5',
       isDisabled: false,
-      outline: false,
       label: 'Shweta',
-      size: 'small',
     },
     },
   component: Button,
@@ -20,26 +25,29 @@ export default {
 
 export const Standard = (args) => <Button {...args} />;
 Standard.args = {
+  varient: 'solid',
   size: 'small',
-  color: 'secondary',
-  label: 'Shweta',
+  color: 'primary',
+  label: 'Button',
   className: 'mr-5',
   isDisabled: false,
-  outline: false,
   loadingText: '',
-  isLoading: false
+  isLoading: false,
+  isLink: false,
 };
 
 export const AllButtons = (args) => (
   <div>
-    <Button size="small" color="primary" label="Primary" className="mr-3"/>
-    <Button color="secondary" className="mr-3" label="Secondary"/>
-    <Button color="primary" className="mr-3" isDisabled label="Disabled "/>
-    <Button outline color="primary" className="mr-3" label="Outline 1"/>
-    <Button outline color="secondary" className="mr-3" label="Outline 2"/>
-    <Button outline color="primary" className="mr-3" isDisabled label="Disabled Outline"/>
+    <Button varient="solid" size="small" color="primary" label="Primary" className="mr-3 mb-3"/>
+    <Button color="secondary" className="mr-3 mb-3" label="Secondary"/>
+    <Button varient="solid-pill" color="secondary" className="mr-3 mb-3" label="Secondary"/>
+    <Button color="primary" className="mr-3 mb-3" isDisabled label="Disabled "/>
+    <Button varient="outline" color="primary" className="mr-3 mb-3" label="Outline 1"/>
+    <Button varient="outline" color="secondary" className="mr-3 mb-3" label="Outline 2"/>
+    <Button varient="outline" color="primary" className="mr-3 mb-3" isDisabled label="Disabled Outline"/>
+    <Button varient="outline-pill" color="primary" className="mr-3 mb-3" isDisabled label="Pill Outline"/>
+    <Button isLink varient="solid" color="primary" className="mr-3 mb-3" label="Link Button"/>
 
-    <Button outline cornerRadius="rounded-full" color="primary" className="mr-3" isDisabled label="Pill Outline"/>
   </div>
 );
 
